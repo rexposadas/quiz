c9bbc2361179d8798b5924d085b7fda9188b29dd
Rex Posadas <rexposadas@gmail.com>
2023-09-25 05:42:37 +0800

    outlined button

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,32 @@
 import 'package:flutter/material.dart';
 
-import 'package:first_app/gradient_container.dart';
+import 'package:first_app/start_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-// With Flutter, you create user interfaces by combining "widgets"
-// You'll learn all about them (and much more) throughout this course!
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // Every custom widget must have a build() method
-  // It tells Flutter, which widgets make up your custom widget
-  // Again: You'll learn all about that throughout the course!
   @override
   Widget build(BuildContext context) {
-    // Below, a bunch of built-in widgets are used (provided by Flutter)
-    // They will be explained in the next sections
-    // In this course, you will, of course, not just use them a lot but
-    // also learn about many other widgets!
     return MaterialApp(
-      title: 'Flutter First App',
+      title: 'Quiz',
       theme: ThemeData(useMaterial3: true),
       home: Scaffold(
-        backgroundColor: Colors.orange,
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter Course'),
-        ),
-        body: const GradientContainer(),
+        body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.blue,
+                  Colors.purple,
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: const StartScreen()),
       ),
     );
   }
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -5,6 +5,26 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return const Text('start screen');
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/images/quiz-logo.png', width: 300),
+          const SizedBox(height: 80.0),
+          const Text(
+            'Let flutter the fund way',
+            style: TextStyle(color: Colors.white, fontSize: 24.0),
+          ),
+          const SizedBox(height: 30.0),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            child: const Text('Start Quiz'),
+          ),
+        ],
+      ),
+    );
   }
 }
