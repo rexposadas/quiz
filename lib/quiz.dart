781844ab9edcc0011e61317f0cce541613266836
Rex Posadas <rexposadas@gmail.com>
2023-09-26 06:47:54 +0800

    done with conditionals

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -13,23 +13,21 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  Widget? activeScreen;
-
-  @override
-  void initState() {
-    activeScreen = StartScreen(switchScreen);
-    super.initState();
-  }
+  var activeScreen = 'start-screen';
 
   void switchScreen() {
     setState(() {
       // calls build() again
-      activeScreen = const QuestionsScreen();
+      activeScreen = 'questions-screen';
     });
   }
 
   @override
   Widget build(context) {
+    final screenWidget = activeScreen == 'start-screen'
+        ? StartScreen(switchScreen)
+        : const QuestionsScreen();
+
     return MaterialApp(
         home: Scaffold(
       body: Container(
@@ -43,7 +41,7 @@ class _QuizState extends State<Quiz> {
             end: Alignment.bottomRight,
           ),
         ),
-        child: activeScreen,
+        child: screenWidget,
       ),
     ));
   }
